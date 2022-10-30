48e8694e93f688aacf8a11ece81d7311f186530f
MacroHongZ <49227696+MacroHongZ@users.noreply.github.com>
2022-10-30 18:51:34 +0800

    Add files via upload

--- DeepLog/setup.py
+++ DeepLog/setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="deeplog",
-    version="1.0",
+    version="1.1",
     author="MacroHongZ",
-    author_email="anghongzhunsix@qq.com",
+    author_email="wanghongzhunsix@qq.com",
+    url='https://github.com/MacroHongZ/DeepLog',
 
     description="Log the deep learning project",
 
